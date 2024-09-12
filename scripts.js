9995a02ded3afe6c8a28c43a9dca4a345b6006d4
Jens Glasbrenner <glasbrennerjens@posteo.de>
2024-09-12 09:45:38 +0200

    Base for the calculator created!

--- scripts.js
+++ scripts.js
@@ -1,27 +1,48 @@
+let firstNnumber;
+let anotherNumber;
+let op = "";
+
 function add(a, b) {
+  let input = document.getElementById("displayNumber").value;
+  alert(input);
   return a + b;
 }
 function subtract(a, b) {
+  let input = document.getElementById("displayNumber").value;
+  alert(input);
   return a - b;
 }
 function multiply(a, b) {
+  let input = document.getElementById("displayNumber").value;
+  alert(input);
   return a * b;
 }
 
 function divide(a, b) {
+  let input = document.getElementById("displayNumber").value;
+  alert(input);
   let result = a / b;
   if (b === 0) {
     return "You can't divide by 0!";
   }
   return result;
 }
-
-function operate(a, b, operator) {
-    const operations = {
-        '+': add,
-        '-': subtract,
-        '*': multiply,
-        '/': divide
-    };
-    return operations[operator](a, b);
+function clear(a, b) {
+  let input = document.getElementById("displayNumber").value;
+  input = "";
+  a = "";
+  b = "";
+  alert(input);
+  console.log(input);
+  return input;
 }
+function operate(a, b, operator) {
+  const operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+  };
+  return operations[operator](a, b);
+}
+
